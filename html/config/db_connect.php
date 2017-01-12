63f6c6f8afe7c942a06de57cccbb69dfe518a925
KimTaeGuk <xoox1020@naver.com>
2017-01-12 15:39:00 +0900

    2017-01-12

--- html/config/db_connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $con=mysqli_connect('localhost','xoox1020', 'xoxo1020', 'xoox1020') or exit ("DB접속시 에러가 발생하였습니다.");
-  $con->set_charset('utf8');
- ?>
--- html/config/mysql_result.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function mysql_result($result, $number, $field){
-		mysqli_data_seek($result, $number);
-		$row=mysqli_fetch_array($result);
-		return $row[$field];
-	}
-?>
