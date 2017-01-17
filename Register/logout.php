08b9ed588c48e3c5e36fda4d0e889acb637921b8
KimTaeGuk <xoox1020@naver.com>
2017-01-17 16:51:00 +0900

    2017-01-17

--- Register/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	session_unset();
-	session_destroy();
-//	echo("<meta http-equiv='Refresh' content='0; URL=../index.php'>");
- 	echo("<meta http-equiv='Refresh' content='0; URL=../index.php'>");
- 
- ?>
