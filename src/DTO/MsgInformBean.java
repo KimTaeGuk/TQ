78b2574c82b0ee5d86ac730471e86e124e96fd1f
KimTaeGuk <xoox1020@naver.com>
2017-02-20 12:50:15 +0900

    2017/02/20(AM)

---
+++ src/DTO/MsgInformBean.java
@@ -0,0 +1,40 @@
+package DTO;
+import java.util.ArrayList;
+public class MsgInformBean {
+	private String comment_id;
+	private String comment_date;
+	private String comment_content;
+	private String board_id;
+	private int board_num;
+
+	public String getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(String comment_id) {
+		this.comment_id = comment_id;
+	}
+	public String getComment_date() {
+		return comment_date;
+	}
+	public void setComment_date(String comment_date) {
+		this.comment_date = comment_date;
+	}
+	public String getComment_content() {
+		return comment_content;
+	}
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+	public String getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(String board_id) {
+		this.board_id = board_id;
+	}	
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+}
